5206a7909d8a0f51e1e4b4ea26ac20b764779334
stasisvisor <stasis_visor.02@icloud.com>
2024-09-20 23:30:00 +0700

    Improved mobile responsiveness

---
+++ replica.js
@@ -0,0 +1,1 @@
+const isPalindrome = (str) => { const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, ‘’); return cleanStr === cleanStr.split(’’).reverse().join(’’); }; console.log(isPalindrome(‘A man, a plan, a canal: Panama’));
